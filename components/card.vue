a402c22b0a03af1d73e2356f119e92437ce8e000
marvimarvv <marvin.tandler@outlook.com>
2023-08-10 19:45:14 +0200

    Updates

--- components/card.vue
+++ components/card.vue
@@ -1,66 +1,71 @@
 <template>
-    <article class="card">
-        <img height="360" width="640" src="../assets/images/fusspflege.png" class="card__image" alt="Screenshot Seite Fußpflege Heike Tandler">
-        <div class="card__body">
-            <h2 class="mb-s">{{heading}}</h2>
-            <ul class="card__list mb-l">
-                <li v-for="item in items" :key="item.bulletpoint">
-                    {{item.bulletpoint}}
-                </li>
-            </ul>
-            <a class="btn" href="https://fusspflegetandler.de" target="blank">Visit site</a>
-        </div>
-    </article>
+  <article class="card">
+    <img
+      height="360"
+      width="640"
+      src="../assets/images/fusspflege.png"
+      class="card__image"
+      alt="Screenshot Seite Fußpflege Heike Tandler"
+    />
+    <div class="card__body">
+      <h2 class="mb-s">{{ heading }}</h2>
+      <ul class="card__list mb-l">
+        <li v-for="item in items" :key="item.bulletpoint">
+          {{ item.bulletpoint }}
+        </li>
+      </ul>
+      <a class="btn" href="https://fusspflegetandler.de" target="blank"
+        >Visit site</a
+      >
+    </div>
+  </article>
 </template>
 
 <script>
 export default {
-    name: 'Card',
-    props: ['heading'],
-    data (){
-        return {
-            items: [
-            { bulletpoint: 'No CSS Framework' },
-            { bulletpoint: 'Sass' },
-            { bulletpoint: 'NPM' },
-            { bulletpoint: 'Brilliant Page Speed Score' },
-            { bulletpoint: 'On Scroll Animations' }
-            ]
-        }
-    }
-}
+  name: "Card",
+  props: ["heading"],
+  data() {
+    return {
+      items: [
+        { bulletpoint: "No CSS Framework" },
+        { bulletpoint: "Sass" },
+        { bulletpoint: "NPM" },
+        { bulletpoint: "Brilliant Page Speed Score" },
+        { bulletpoint: "On Scroll Animations" }
+      ]
+    };
+  }
+};
 </script>
-
 
 <style lang="scss">
 .card {
-    position: relative;
-    box-shadow: $box-shadow-outset-l;
-    transition: all 0.2s ease;
-    background-color: #e6e7ee;
-    display: flex;
-    flex-direction: column;
-    min-width: 0;
-    word-wrap: break-word;
-    background-clip: border-box;
-    border: 0.0625rem solid #D1D9E6;
-    border-radius: $border-radius;
+  position: relative;
+  box-shadow: $box-shadow-outset-l;
+  transition: all 0.2s ease;
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-clip: border-box;
+  border-radius: $border-radius;
 
-    &__image {
-        max-width: 100%;
-        height: auto;
-        border-top-left-radius: 0.55rem;
-        border-top-right-radius: 0.55rem;
-    }
+  &__image {
+    max-width: 100%;
+    height: auto;
+    border-top-left-radius: 0.55rem;
+    border-top-right-radius: 0.55rem;
+  }
 
-    &__body {
-        flex: 1 1 auto;
-        padding: 3rem 1.5rem;
-    }
+  &__body {
+    flex: 1 1 auto;
+    padding: 3rem 1.5rem;
+  }
 
-    &__list {
-        padding-left: .5rem;
-        list-style-position: inside;
-    }
+  &__list {
+    padding-left: 0.5rem;
+    list-style-position: inside;
+  }
 }
 </style>
--- pages/skills.vue
+++ pages/skills.vue
@@ -4,84 +4,184 @@
     <ul class="skills">
       <li class="skills__list-item">
         <figure class="skills__row">
-          <div class="skills__icon skills__icon--1" aria-label="Skills icon with animated patricles">
-            <div class="skills__icon__particle skills__icon--1__particle--1 animate fade delay-03"></div>
-            <div class="skills__icon__particle skills__icon--1__particle--2 animate fade delay-18"></div>
-            <div class="skills__icon__particle skills__icon--1__particle--3 animate fade delay-36"></div>
-            <div class="skills__icon__particle skills__icon--1__particle--4 animate fade delay-24"></div>
-            <div class="skills__icon__particle skills__icon--1__particle--5 animate fade delay-09"></div>
-            <div class="skills__icon__particle skills__icon--1__particle--6 animate fade delay-42"></div>
-            <div class="skills__icon__particle skills__icon--1__particle--7 animate fade delay-12"></div>
-            <div class="skills__icon__particle skills__icon--1__particle--8 animate fade delay-06"></div>
+          <div
+            class="skills__icon"
+            aria-label="Animated HTML icon fading in and out"
+          >
+            <div
+              class="skills__icon__html skills__icon__html--1 animate fade delay-03"
+            ></div>
+            <div
+              class="skills__icon__html skills__icon__html--2 animate fade delay-06"
+            ></div>
+            <div
+              class="skills__icon__html skills__icon__html--3 animate fade delay-09"
+            ></div>
           </div>
           <figcaption class="skills__text">
-            <h2 class="skills__heading mb-s">HTML & CSS - Standards for the win!</h2>
+            <h2 class="skills__heading mb-s">
+              HTML & CSS - Standards for the win!
+            </h2>
             <p class="skills__paragraph">
-              I have a passion for writing semantic HTML and modern CSS. Writing clean, short and understandable code with the standards at our hands has some
-              serious benefits for various reasons. One of them is improving accessibility so that the experiences we create are inclusive and take into account people's different abilities.
-              Another advantage is that the more standard compliant you are working, the less you make yourself dependent on third party solutions. Plus, your code is executable on all the major web platforms
-              (as you know that is not always the case but we are getting there). There are still so many more reasons like SEO, maintainability and so on...
+              I have a passion for writing semantic HTML and modern CSS. Writing
+              clean, short and understandable code with the standards at our
+              hands has some serious benefits for various reasons. One of them
+              is improving accessibility so that the experiences we create are
+              inclusive and take into account people's different abilities.
+              Another advantage is that the more standard compliant you are
+              working, the less you make yourself dependent on third party
+              solutions. Plus, your code is executable on all the major web
+              platforms (as you know that is not always the case but we are
+              getting there). There are still so many more reasons like SEO,
+              maintainability and so on...
             </p>
           </figcaption>
         </figure>
       </li>
       <li class="skills__list-item">
-          <figure class="skills__row">
-            <div class="skills__icon skills__icon--2" aria-label="Skills icon with animated patricles">
-              <div class="skills__icon__particle skills__icon--1__particle--1 animate fade delay-03"></div>
-            </div>
-            <figcaption class="skills__text">
-              <h2 class="skills__heading mb-s">Designing</h2>
-              <p class="skills__paragraph">
-                For me, designing is about a lot of different aspects of the final product. Having aesthetically pleasing visuals, delighting the user
-                and guiding him are the key elements that drive my designs. Design is not just about how it looks though. To have a satisfying user experience, it is important to
-                implement known behavioral patterns into the product and to not overburden the user with complexity.
-              </p>
-            </figcaption>
-          </figure>
+        <figure class="skills__row">
+          <div
+            class="skills__icon"
+            aria-label="Animated blurred dots fading in and out"
+          >
+            <div
+              class="skills__icon__particle skills__icon__particle--1 animate fade delay-03"
+            ></div>
+            <div
+              class="skills__icon__particle skills__icon__particle--2 animate fade delay-18"
+            ></div>
+            <div
+              class="skills__icon__particle skills__icon__particle--3 animate fade delay-36"
+            ></div>
+            <div
+              class="skills__icon__particle skills__icon__particle--4 animate fade delay-24"
+            ></div>
+            <div
+              class="skills__icon__particle skills__icon__particle--5 animate fade delay-09"
+            ></div>
+            <div
+              class="skills__icon__particle skills__icon__particle--6 animate fade delay-42"
+            ></div>
+            <div
+              class="skills__icon__particle skills__icon__particle--7 animate fade delay-12"
+            ></div>
+            <div
+              class="skills__icon__particle skills__icon__particle--8 animate fade delay-06"
+            ></div>
+          </div>
+          <figcaption class="skills__text">
+            <h2 class="skills__heading mb-s">Designing</h2>
+            <p class="skills__paragraph">
+              For me, designing is about a lot of different aspects of the final
+              product. Having aesthetically pleasing visuals, delighting the
+              user and guiding him are the key elements that drive my designs.
+              Design is not just about how it looks though. To have a satisfying
+              user experience, it is important to implement known behavioral
+              patterns into the product and to not overburden the user with
+              complexity.
+            </p>
+          </figcaption>
+        </figure>
       </li>
       <li class="skills__list-item">
-        <div class="skills__icon skills__icon--3" aria-label="Skills icon with animated patricles">
-          <figure class="skills__row">
-            <figcaption class="skills__text">
-              <h2 class="skills__heading mb-s">Jamstack & Wordpress</h2>
-              <p class="skills__paragraph">
-                As a frontend designer, I am also keen on implementing my designs and make them a reality. For implementation I like to try out
-                things to see if they fit my workflow and deliver clean code & performance. I have experience in working with Wordpress and currently I am trying out Nuxt.js by building
-                this website. In the future I am looking forward to get more into design systems and Next.js.
-              </p>
-            </figcaption>
-          </figure>
-        </div>
-      </li><li class="skills__list-item">
-        <div class="skills__icon skills__icon--4" aria-label="Skills icon with animated patricles">
-          <figure class="skills__row">
-            <figcaption class="skills__text">
-              <h2 class="skills__heading mb-s">Accessibility</h2>
-              <p class="skills__paragraph">
-                Being the one that is creating the user experience also means making sure that everyone gets to enjoy it no matter his/her/their abilities.
-                Creating the most fanciest website has no use if you limit the audience that is able to use it. This is why it is important to me to use techniques
-                that make sure we don't exclude people from digital experiences. Paying attention to contrast, writing semantic HTML and making sure the keyboard navigation is working
-                as it should are a few things that can already cover a lot of the do's there are for creating inclusive experiences but are often overlooked.
-              </p>
-            </figcaption>
-          </figure>
-        </div>
+        <figure class="skills__row">
+          <div
+            class="skills__icon"
+            aria-label="Animated curly brackets fading in and out"
+          >
+            <div class="skills__icon__coding animate fade delay-15"></div>
+          </div>
+          <figcaption class="skills__text">
+            <h2 class="skills__heading mb-s">Jamstack & Wordpress</h2>
+            <p class="skills__paragraph">
+              As a frontend designer, I am also keen on implementing my designs
+              and make them a reality. For implementation I like to try out
+              things to see if they fit my workflow and deliver clean code &
+              performance. I have experience in working with Wordpress and
+              currently I am trying out Nuxt.js by building this website. Also,
+              I am looking more into design systems and am realizing another
+              project with Next.js at the moment.
+            </p>
+          </figcaption>
+        </figure>
+      </li>
+      <li class="skills__list-item">
+        <figure class="skills__row">
+          <div
+            class="skills__icon"
+            aria-label="Skills icon with animated patricles"
+          >
+            <!-- Generator: Adobe Illustrator 27.6.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+            <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 122.9 122.9"
+              style="enable-background:new 0 0 122.9 122.9;"
+              xml:space="preserve"
+              class="skills__icon__accessibility"
+            >
+              <path
+                class="skills__icon__accessibility__person animate fade delay-06"
+                d="M61,74.2l-9,24.7c-0.4,1.3-1.4,2.3-2.6,2.8c-2.5,1.2-5.5,0.1-6.6-2.4c-0.6-1.2-0.6-2.6-0.2-3.8l6.2-17.3
+	c0.5-1.3,0.9-2.6,1.2-4c0.3-1.4,0.4-2.8,0.5-4.2c0.2-2.5,0.4-5.3,0.5-7.9s0.2-5.2,0.3-7.3c0.1-2.6-0.6-2.8-2.7-3.3l-0.4-0.1l-18-3.4
+	c-1.3-0.2-2.5-1-3.2-2.1c-1.6-2.3-1-5.4,1.3-7c1.1-0.8,2.4-1,3.8-0.8l19.3,3.6c0.8,0.1,1.5,0.2,2.3,0.2c2.4,0.3,4.8,0.5,7.2,0.5
+	c3-0.1,6-0.3,8.9-0.7c0.9-0.1,1.8-0.2,2.6-0.3l18.2-3.4c1.3-0.3,2.6,0,3.8,0.7c2.3,1.5,2.9,4.7,1.3,6.9c0,0,0,0,0,0.1
+	c-0.8,1.1-1.9,1.8-3.2,2.1l-17.4,3.3c-0.6,0.1-1.1,0.2-1.6,0.3c-1.8,0.3-2.7,0.5-2.6,3.1c0.1,1.9,0.3,4.2,0.6,6.5
+	c0.3,2.8,0.8,5.7,1.3,8.4c0.3,1.8,0.6,3.2,1,4.6s0.8,2.8,1.4,4.4l6.1,16.9c0.9,2.6-0.4,5.5-3,6.4c-1.3,0.5-2.6,0.4-3.8-0.2
+	c-1.2-0.6-2.1-1.6-2.6-2.8l-9-24.7l-1-1.8L61,74.2L61,74.2z M61.4,20.7c4.9,0,8.8,3.9,8.8,8.8c0,4.9-3.9,8.8-8.8,8.8
+	c-4.9,0-8.8-3.9-8.8-8.8c0-2.3,0.9-4.6,2.6-6.2C56.9,21.6,59.1,20.7,61.4,20.7z"
+              />
+              <g>
+                <path
+                  class="skills__icon__accessibility__circle animate fade delay-12"
+                  d="M61.4,10c13.2,0,26.4,5,36.4,15.1c9.6,9.6,15,22.7,15,36.3c0,13.6-5.5,26.7-15.1,36.4c-10,10-23.2,15-36.3,15
+		c-13.2,0-26.4-5-36.4-15.1C4.9,77.6,5,45,25.1,25C35.1,15,48.2,10,61.4,10 M61.4,1C45.3,1,30.1,7.2,18.7,18.6C7.3,30,1,45.1,1,61.3
+		c0,16.1,6.2,31.3,17.6,42.8c11.4,11.5,26.6,17.8,42.8,17.8c16.1,0,31.2-6.3,42.7-17.6c11.4-11.4,17.8-26.6,17.8-42.7
+		c0-15.9-6.4-31.5-17.6-42.7C92.7,7.3,77.5,1,61.4,1L61.4,1z"
+                />
+              </g>
+            </svg>
+
+            <div class="accessibility-icon-container">
+              <div
+                class="accessibility-icon"
+                style="clip-path: url(#accessibility-clip);"
+              ></div>
+            </div>
+          </div>
+          <figcaption class="skills__text">
+            <h2 class="skills__heading mb-s">Accessibility</h2>
+            <p class="skills__paragraph">
+              Being the one that is creating the user experience also means
+              making sure that everyone gets to enjoy it no matter his/her/their
+              abilities. Creating the most fanciest website has no use if you
+              limit the audience that is able to use it. This is why it is
+              important to me to use techniques that make sure we don't exclude
+              people from digital experiences. Paying attention to contrast,
+              writing semantic HTML and making sure the keyboard navigation is
+              working as it should are a few things that can already cover a lot
+              of the do's there are for creating inclusive experiences but are
+              often overlooked.
+            </p>
+          </figcaption>
+        </figure>
       </li>
     </ul>
   </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss">
 .skills {
   display: flex;
   flex-direction: column;
+  row-gap: 8rem;
 
   &__list-item {
     list-style: none;
@@ -89,9 +189,9 @@ export default {
 
   &__row {
     display: flex;
-    gap: 6rem;
+    column-gap: 6rem;
 
-    @media (max-width: 767px) {
+    @media (max-width: 900px) {
       flex-direction: column;
     }
   }
@@ -102,9 +202,42 @@ export default {
     min-height: 30vh;
     aspect-ratio: 1/1;
     align-self: flex-start;
+    filter: blur(10px);
 
-    @media (max-width: 767px) {
+    @media (max-width: 900px) {
       align-self: center;
+    }
+
+    &__html {
+      background-color: $color-primary;
+      position: absolute;
+      width: 100%;
+      top: 0;
+
+      &--1 {
+        clip-path: polygon(22% 21%, 9% 50%, 25% 74%, 16% 74%, 0% 50%, 14% 21%);
+        left: 0;
+        aspect-ratio: 1/1;
+      }
+
+      &--2 {
+        clip-path: polygon(
+          78% 21%,
+          91% 50%,
+          75% 74%,
+          84% 74%,
+          100% 50%,
+          86% 21%
+        );
+        right: 0;
+        aspect-ratio: 1/1;
+      }
+
+      &--3 {
+        clip-path: polygon(71% 6%, 36% 88%, 29% 88%, 63% 6%);
+        left: 0;
+        aspect-ratio: 1/1;
+      }
     }
 
     &__particle {
@@ -112,10 +245,7 @@ export default {
       aspect-ratio: 1/1;
       border-radius: 50%;
       background-color: $color-primary;
-      filter: blur(15px);
-    }
 
-    &--1__particle {
       &--1 {
         top: 5%;
         left: 5%;
@@ -164,11 +294,45 @@ export default {
         width: 15%;
       }
     }
+
+    &__coding {
+      position: absolute;
+      width: 100%;
+      aspect-ratio: 1/1;
+      color: $color-primary;
+      line-height: 1;
+
+      &::after {
+        content: "}";
+        position: absolute;
+        font-size: 30rem;
+        left: 60%;
+        top: 0%;
+      }
+
+      &::before {
+        content: "{";
+        position: absolute;
+        font-size: 30rem;
+        right: 60%;
+        top: 0%;
+      }
+    }
+
+    &__accessibility {
+      &__person,
+      &__circle {
+        fill: $color-primary;
+      }
+    }
   }
 
   &__text {
     flex-basis: 75ch;
-    flex-shrink: 1;
+
+    @media (max-width: 900px) {
+      flex-basis: unset;
+    }
   }
 
   &__paragraph {
@@ -181,7 +345,6 @@ export default {
   }
 }
 
-
 /* ==========================================================================
 Animation System by Neale Van Fleet from Rogue Amoeba (modified) https://css-tricks.com/a-handy-little-system-for-animated-entrances-in-css/
 ========================================================================== */
@@ -189,7 +352,7 @@ Animation System by Neale Van Fleet from Rogue Amoeba (modified) https://css-tri
   animation-duration: 3s;
   animation-delay: 0.5s;
   animation-name: animate-fade;
-  animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
+  animation-timing-function: cubic-bezier(0.26, 0.53, 0.74, 1.48);
   animation-fill-mode: backwards;
 }
 
@@ -202,8 +365,12 @@ Animation System by Neale Van Fleet from Rogue Amoeba (modified) https://css-tri
 }
 
 @keyframes animate-fade {
-  0% { opacity: 0; }
-  100% { opacity: 1; }
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 
 /* Animation Delays */
